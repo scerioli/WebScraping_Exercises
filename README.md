ad6ae920b5db0de05a902910c63531d63e450dac
Sara Cerioli <cerioli@adference.com>
2020-11-23 22:18:43 +0100

    Modified the README; added the code for webscraping in case of list that requires goin over many pages

--- README.md
+++ README.md
@@ -2,9 +2,14 @@
 
 In general, I was following this blog as a starting point: 
 https://www.analyticsvidhya.com/blog/2017/03/beginners-guide-on-web-scraping-in-r-using-rvest-with-hands-on-knowledge/
+And this:
+https://www.r-bloggers.com/2020/07/tutorial-web-scraping-of-multiple-pages-using-r/
 1. Check this WONDERFUL website: https://flukeout.github.io/#
     Here you can learn some basics of CSS that can really be helpful, while having fun with a nice game :D
 2. Find a page that interests you to analyse deeper. Here I used the page from IMDb for the most rated movies of 2016
 3. Open the developer's view of the page: Usually, something in the top corner of the browser, hidden somewhere in the "Extra tools". It is usually called "Developer tools" or similar, I guess it depends on the browser
 4. Check the elements you need for scraping. You can either go into the Java code and check what you need (it will be selected on the page), or you can click on with the left click of your mouse on "Inspect" and you will find the corresponding piece of code of the item you selected to be inspected
 5. With the help of the library "rvest", one can use simple commands to load the elements in RStudio
+
+Interesting for the future: Screenplays of many IMDb movies
+https://www.imsdb.com/
--- WebscrapingForDummies.r
+++ WebscrapingForDummies.r
@@ -17,8 +17,45 @@ rank_runtime <- html_text(rank_runtime_html)
 rank_genre_html <- html_nodes(webpage, '.text-muted span.genre')
 rank_genre <- html_text(rank_genre_html)
 
+#----------------------------------------------------------------------------- #
 
+# Define the url of the page you want to scrape
+# The %s at the end is due to the fact that we are going to loop over the pages
+new_url <- "https://www.imdb.com/list/ls058011111/?sort=list_order,asc&mode=detail&page=%s"
 
+table <- data.table()
+dt <- data.table()
 
-# Interesting for the future: Screenplays of many IMDb movies
-# https://www.imsdb.com/
+i <- 1
+
+while (i < 11) {
+  # Read the page with the increasing index
+  new_webpage <- read_html(sprintf(new_url, i))
+  
+  # Access the data and transform them to text
+  # Rank of the actress/actor
+  rank_html <- html_nodes(new_webpage, '.text-primary')
+  rank <- html_text(rank_html)
+  rank <- as.numeric(gsub(",", "", rank))
+  # Name of the actress/actor
+  name_html <- html_nodes(new_webpage, '.lister-item-header a')
+  name <- gsub(pattern = "\n", replacement = "", x = html_text(name_html))
+  # Gender of the actress/actor
+  gender_html <- html_nodes(new_webpage, '.text-muted.text-small')
+  gender <- gsub(pattern = " ", replacement = "", x = html_text(gender_html))
+  gender <- gsub("\n", "", x = gender)
+  gender <- sapply(strsplit(x = gender, split = "|", fixed = TRUE), '[', 1)
+  
+  # Make a table of ouf the data and rbind them together
+  table <- data.table(name = name,
+                      gender = ifelse(gender == "Actress", "Female", "Male"),
+                      rank = rank)
+  dt <- rbind(dt, table)
+  
+  i <- i + 1
+}
+
+ggplot(data = dt) +
+  geom_bar(aes(y = rank, fill = gender), orientation = "y") +
+  coord_trans(y = "log") + 
+  ggtitle("Rank of actresses and actors in log scale")
